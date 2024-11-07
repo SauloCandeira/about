365d5fb08b3d3b541279ae2c59f3147ec0179c75
Saulo Candeira <saulocandeira@hotmail.com>
2024-11-07 11:09:19 -0300

    News

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -8,7 +8,7 @@ import './Services.css';
 const Services: React.FC = () => {
   return (
     <div className="services-container">
-      <h1 className="services-title">Nossos Serviços</h1>
+      <h1 className="services-title">Serviços</h1>
 
       <Tabs>
         <TabList className="react-tabs__tab-list">
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Header from '../../components/Header/Header';
 import Projects from '../../components/Projects/Projects';
 import './Home.css'; // Arquivo de estilos
-import Courses from '../../components/Courses/Courses';
+// import Courses from '../../components/Courses/Courses';
 import FounderCard from '../../components/FounderCard/FounderCard';
-import Marketplace from '../../components/Marketplace/Marketplace';
+// import Marketplace from '../../components/Marketplace/Marketplace';
 import Services from '../../components/Services/Services';
 
 const Home: React.FC = () => {
